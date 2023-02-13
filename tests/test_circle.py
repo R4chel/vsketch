9b5d9224597e775e04967623072f5ed5a195cd5d
R4chel <rae84@cornell.edu>
2023-02-13 13:40:25 -0500

    add test

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -20,6 +20,10 @@ def test_circle_radius(vsk: vsketch.Vsketch) -> None:
     assert line_count_equal(vsk, 1)
     assert bounds_equal(vsk, -5, -5, 5, 5)
 
+def test_circle_radius_mode(vsk: vsketch.Vsketch) -> None:
+    vsk.circle(0, 0, radius=5, mode="radius")
+    assert line_count_equal(vsk, 1)
+    assert bounds_equal(vsk, -5, -5, 5, 5)
 
 def test_circle_diameter(vsk: vsketch.Vsketch) -> None:
     vsk.detail(0.01)  # make sure we have a tight bound match
